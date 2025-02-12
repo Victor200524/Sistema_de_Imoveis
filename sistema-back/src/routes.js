8e0c293bb8dc7c3bdf15fa78e21ea41a7471e080
Victor200524 <victor.donaire@hotmail.com>
2025-02-12 19:57:11 -0300

    Projeto Front-end Finalizado do Sistema de Imóveis

--- sistema-back/src/routes.js
+++
@@ -1,8 +0,0 @@
-import {Router} from 'express';
-import UserController from './Controllers/UserController';
-
-const router = Router(); // Inicializa o Router
-
-router.post('/createUsers', UserController.createUser); // Rota para criar um usuário
-
-export {router}; // Exporta o router
